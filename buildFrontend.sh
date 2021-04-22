4afcddc1f82605e86c5fa8cf51305cdae49f0acb
Sylvain Doignon <sylvain@doignon.fr>
2021-04-22 18:26:03 +0100

    streamline bash script

--- buildFrontend.sh
+++ buildFrontend.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+set -eu
 
 echo "[1] Build wasm"
-cd wasm || exit
-GOOS=js GOARCH=wasm go build -o wasm.wasm wasm.go
-cd .. || exit
+wasmPayloadFile=$(mktemp)
+trap 'rm -f $wasmPayloadFile' ERR EXIT
+GOOS=js GOARCH=wasm go build -o "$wasmPayloadFile" wasm/wasm.go
 
 echo "[2] Pack wasm into javascript"
-
-echo 'var wasmPayloadInlineURL = `data:application/wasm;base64,' > frontend/js/wasmPayload.js
-base64 wasm/wasm.wasm >> frontend/js/wasmPayload.js
-echo '`' >> frontend/js/wasmPayload.js
+wasmJsPayloadFile="frontend/js/wasmPayload.js"
+echo 'const wasmPayloadInlineURL = `data:application/wasm;base64,' > "$wasmJsPayloadFile"
+base64 "$wasmPayloadFile" >> "$wasmJsPayloadFile"
+echo '`' >> "$wasmJsPayloadFile"
 
 echo "[3] Done. You can upload the frontend folder to your server or use it locally"
