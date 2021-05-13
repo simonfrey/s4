38372aeb62b279d4241c7b5cad7c4d60466ea0e6
Sylvain Doignon <sylvain@doignon.fr>
2021-05-13 18:32:29 +0100

    delete all styles, revamp JS frontend

--- frontend/js/encrypt.js
+++ frontend/js/encrypt.js
@@ -1,71 +1,59 @@
-function doEncrypt() {
-  var thresholdEl = document.getElementById('threshold');
-  var sharesEl = document.getElementById('shares');
-  
-  // validation
-  if (sharesEl.valueAsNumber <= 1) {
-    sharesEl.value = 2;
-  }
+function fillEncryptOutputs(values) {
+    const output = document.getElementById("encryptOutput")
+    output.innerHTML = ""
 
-  if (thresholdEl.valueAsNumber <= 1) {
-    thresholdEl.value = 2;
-  }
-
-  // ensure threshold isn't higher than shares
-  thresholdEl.setAttribute("max", sharesEl.valueAsNumber);
-  if (sharesEl.valueAsNumber < thresholdEl.valueAsNumber) {
-    thresholdEl.value = sharesEl.value;
-  }
-
-  // snag values from the dom
-  var threshold = thresholdEl.valueAsNumber;
-  var shares = sharesEl.valueAsNumber;
-  var useAES = document.getElementById('useAES').checked;
-  var input = document.getElementById('input').value;
-
-  // handle no input
-  if (input == "") {
-    return fillOutputs(Array(shares).fill(""));
-  }
-
-  // retry if wasm didn't load yet.
-  if (typeof Distribute_fours == "undefined") {
-    setTimeout(doEncrypt, 3000);
-    return setError('WASM not loaded, retrying...');
-  }
-
-  // do it!
-  var res = Distribute_fours(
-    String(btoa(input)),
-    Number(shares),
-    Number(threshold),
-    Boolean(useAES)
-  );
-
-  // update dom w/ results
-  if (typeof res === 'string'){
-    setError(res)
-    fillOutputs(Array(shares).fill(""));
-  } else {
-    setError('');
-    fillOutputs(res);
-  }
+    values.forEach((value) => {
+        const textarea = document.createElement("textarea")
+        textarea.readOnly = true
+        textarea.innerHTML = value
+        output.appendChild(textarea)
+    })
 }
 
-function fillOutputs(values) {
-  var out = document.getElementById("outputs");
-  out.innerText = '';
+function doEncrypt() {
 
-  values.forEach(function(o) {
-    var div = document.createElement('div');
-    div.className = "column";
+    const thresholdEl = document.getElementById("encryptThreshold")
+    const sharesEl = document.getElementById("encryptShares")
 
-    var ta = document.createElement("textarea");
-    ta.className = 'textarea';
-    ta.readOnly = 'readonly';
-    ta.innerHTML = o;
+    // Validation
+    if (sharesEl.valueAsNumber <= 1) {
+        sharesEl.value = 2
+    }
+    if (thresholdEl.valueAsNumber <= 1) {
+        thresholdEl.value = 2
+    }
 
-    div.appendChild(ta);
-    out.appendChild(div);
-  });
+    // Ensure threshold isn't higher than shares
+    thresholdEl.setAttribute("max", sharesEl.valueAsNumber)
+    if (sharesEl.valueAsNumber < thresholdEl.valueAsNumber) {
+        thresholdEl.value = sharesEl.value
+    }
+
+    // Snag values from the DOM
+    const threshold = thresholdEl.valueAsNumber
+    const shares = sharesEl.valueAsNumber
+    const useAES = document.getElementById("encryptUseAES").checked
+    const input = document.getElementById("encryptInput").value
+
+    // Handle no input
+    if (input === "") {
+        return fillEncryptOutputs(Array(shares).fill(""))
+    }
+
+    // Do it!
+    const res = Distribute_fours(
+        btoa(input),
+        shares,
+        threshold,
+        useAES
+    )
+
+    // Update DOM with results
+    if (typeof res === "string") {
+        setError(res)
+        fillEncryptOutputs(Array(shares).fill(""))
+    } else {
+        setError("")
+        fillEncryptOutputs(res)
+    }
 }
