1381eaf9b3daffc9d176de96762527e8bd44dc38
Simon <mailman@mailbox.org>
2024-02-22 09:25:52 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -2,22 +2,27 @@
 
 set -eu
 
+mkdir build || true
+
 echo "[1] Build wasm"
 wasmPayloadFile=$(mktemp)
 trap 'rm -f $wasmPayloadFile' ERR EXIT
 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o "$wasmPayloadFile" wasm/wasm.go
 
 echo "[2] Pack wasm into javascript"
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" frontend/js/wasm_exec.js
+
 wasmJsPayloadFile="frontend/js/wasmPayload.base64"
+rm $wasmJsPayloadFile || true
 echo -n 'const wasmPayloadInlineURL = `data:application/wasm;base64, ' > "${wasmJsPayloadFile}"
 base64 "$wasmPayloadFile" >> "${wasmJsPayloadFile}"
 echo '`' >> "${wasmJsPayloadFile}"
 JsPayloadConcatenatedFile="frontend/js/jsPayloads.js"
+rm $JsPayloadConcatenatedFile || true
 cat frontend/js/*.js > "${JsPayloadConcatenatedFile}"
 cat "${wasmJsPayloadFile}" >> "${JsPayloadConcatenatedFile}"
 
 echo "[3] Pack css & javascript into a single HTML file"
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" frontend/js/wasm_exec.js
 mkdir -p build
 htmlTemplate="frontend/index.template.html"
 CSSPayloadFile="frontend/css/style.css"
